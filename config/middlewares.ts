0667552c6b890fcba50f709e4633b05bdd7b0e54
Khayalala <khayala.jafar@gmail.com>
2025-01-27 17:33:50 +0400

    minor_change

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,10 +8,25 @@ module.exports = [
         "https://wander-client.onrender.com", 
       ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      headers: ["Content-Type", "Authorization"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      credentials: true,
     },
   },
   "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https://wander-client.onrender.com"],
+          "img-src": ["'self'", "data:", "blob:", "https://wander-client.onrender.com"],
+          "media-src": ["'self'", "data:", "blob:", "https://wander-client.onrender.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
